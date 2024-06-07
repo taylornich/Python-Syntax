03bbf9fa69e992dd793bbafc2a7a61dd683d1526
taylornich <taylormanhard15@gmail.com>
2024-06-06 22:01:31 -0700

    assignment 1 question 1

---
+++ showcase_your_dance_moves.py
@@ -0,0 +1,18 @@
+# question 1 task 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+
+else:
+    print("Take an umbrella!")
+
+
+# question 1 task 2
+x = input("How are you feeling today?")
+
+if x == "happy":
+    print("That's great to hear!")
+
+if x == "sad":
+   print("I hope your day gets better!")
